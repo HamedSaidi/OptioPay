ec4842b518694bc94fbc40002b5f32e04a1a7741
Saidi <saidi@mediapeers.com>
2019-05-28 22:05:27 +0200

    fix: scan for roman number with two letter before scanning for single letters

--- RomanNumerals.js
+++ RomanNumerals.js
@@ -1,10 +1,12 @@
 class RomanNumerals {
   static get decimals() {
-    return [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    // the order is critical to the algorithm of fromRoman and toRoman
+    return [900, 1000, 400, 500, 90, 100, 40, 50, 9, 10, 4, 5, 1]
   }
 
   static get romans() {
-    return ['M', 'CM','D','CD','C', 'XC', 'L', 'XL', 'X','IX','V','IV','I']
+    // the order is critical to the algorithm of fromRoman and toRoman
+    return ['CM','M', 'CD', 'D', 'XC', 'C', 'XL', 'L', 'IX', 'X', 'IV', 'V','I']
   }
 
   static fromRoman(str = '') {
